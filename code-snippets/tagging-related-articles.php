11f567610a3ce4ed874c818d658f16d7abdc162f
pritaeas <pritaeas@hotmail.com>
2012-11-24 11:38:06 +0100

    Stub for new example.

---
+++ code-snippets/tagging-related-articles.php
@@ -0,0 +1,10 @@
+<?php
+    # execute sql dump manually
+    # connect with pdo
+
+    # get article list with tags
+    # get article with top related articles
+
+    # insert new article with tags
+    # update existing article with new tags
+?>
