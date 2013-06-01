a97d27472f21f8c62351473852bc797bfe883277
pritaeas <pritaeas@hotmail.com>
2013-06-01 13:48:19 +0200

    Tabs to spaces.

--- code-snippets/html-select-mysql.php
+++ code-snippets/html-select-mysql.php
@@ -1,33 +1,33 @@
 <?php
-	$dbLink = mysql_connect('localhost', 'myuser', 'mypassword');
-	if (!$dbLink)
+    $dbLink = mysql_connect('localhost', 'myuser', 'mypassword');
+    if (!$dbLink)
     {
-		echo '<p>There was an error connecting to the database!</p>';
-		die();
-	}
+        echo '<p>There was an error connecting to the database!</p>';
+        die();
+    }
 
-	if (!mysql_select_db('mydatabase'))
+    if (!mysql_select_db('mydatabase'))
     {
-		echo '<p>There was an error selecting the database!</p>';
-		mysql_close($dbLink);
-		die();
-	}
+        echo '<p>There was an error selecting the database!</p>';
+        mysql_close($dbLink);
+        die();
+    }
 
-	$query = 'SELECT `mycolumn` FROM `mytable` ORDER BY `mycolumn`';
-	$result = mysql_query($query, $dbLink);
-	if (!$result)
+    $query = 'SELECT `mycolumn` FROM `mytable` ORDER BY `mycolumn`';
+    $result = mysql_query($query, $dbLink);
+    if (!$result)
     {
-		echo '<p>There was an error in your query!</p>';
-	}
-	else
+        echo '<p>There was an error in your query!</p>';
+    }
+    else
     {
         $select = '<select>';
         while ($record = mysql_fetch_array($result)) {
             $select .= "<option value=\"{$record['mycolumn']}\">{$record['mycolumn']}</option>";
         }
         $select .= '</select>';
-		mysql_free_result($result);
-	}
+        mysql_free_result($result);
+    }
 
-	mysql_close($dbLink);
+    mysql_close($dbLink);
 ?>
